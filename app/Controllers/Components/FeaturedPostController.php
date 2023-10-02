34616d7462111f20c35f4d266b211a548804a09d
ducng99 <tom@ducng.dev>
2023-10-03 10:20:49 +1300

    Fix indent

--- app/Controllers/Components/FeaturedPostController.php
+++ app/Controllers/Components/FeaturedPostController.php
@@ -7,16 +7,16 @@ use App\Libraries\Storyblok;
 
 class FeaturedPostController extends BaseController
 {
-	public function show(string $postID)
-	{
-		$story = $this->storyblok->client->getStoryByUuid($postID)->getBody();
-		$componentModelClass = Storyblok::getModelFromComponent($story['story']['content']['component']);
-		$component = new $componentModelClass($story['story']['content']);
+    public function show(string $postID)
+    {
+        $story = $this->storyblok->client->getStoryByUuid($postID)->getBody();
+        $componentModelClass = Storyblok::getModelFromComponent($story['story']['content']['component']);
+        $component = new $componentModelClass($story['story']['content']);
 
-		return view('components/featured_post', [
-			'component' => $component,
-			'story' => $story['story'],
-			'postLoaded' => true,
-		]);
-	}
+        return view('components/featured_post', [
+            'component' => $component,
+            'story' => $story['story'],
+            'postLoaded' => true,
+        ]);
+    }
 }
