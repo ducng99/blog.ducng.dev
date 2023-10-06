fb69d3fc246821ce38ddf5fad1b68f743f195d82
ducng99 <tom@ducng.dev>
2023-10-07 12:53:44 +1300

    Fix site name not displayed on navbar without logo

--- app/Views/components/navbar/navbar.php
+++ app/Views/components/navbar/navbar.php
@@ -11,13 +11,15 @@
         <a class="flex items-center gap-x-4 mr-auto" href="<?= base_url() ?>" title="Home">
             <? if (!empty($component->logo['filename'])) : ?>
                 <img src="<?= esc($component->logo['filename'], 'attr') ?>" alt="<?= esc($component->logo['alt'], 'attr') ?>" title="<?= esc($component->logo['title'], 'attr') ?>" class="w-16 h-16" />
-                <span class="text-4xl font-bold"><?= esc($component->name) ?></span>
             <? endif; ?>
+            <span class="text-4xl font-bold"><?= esc($component->name) ?></span>
         </a>
         <div class="flex gap-2">
             <?
-            foreach ($component->navs as $nav_item) :
-                switch ($nav_item->component):
+            foreach ($component->navs as $nav_item)
+            {
+                switch ($nav_item->component)
+                {
                     case "nav_link":
                         echo view("components/navbar/nav_link", ['component' => $nav_item]);
                         break;
@@ -26,8 +28,8 @@
                         break;
                     default:
                         break;
-                endswitch;
-            endforeach;
+                }
+            }
             ?>
         </div>
     </div>
