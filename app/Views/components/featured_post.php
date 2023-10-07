15a15d617243a94d637ab5fa6bdf9a5f9fe60ab0
ducng99 <tom@ducng.dev>
2023-10-07 14:41:37 +1300

    Change some colours

--- app/Views/components/featured_post.php
+++ app/Views/components/featured_post.php
@@ -7,13 +7,13 @@
      */
     ?>
     <?= $component->_editable ?>
-    <div class="bg-primary dark:bg-neutral-900 h-64 rounded-md flex flex-col">
+    <div class="bg-primary dark:bg-neutral-800 h-64 rounded-md flex flex-col">
         <div class="pt-5 px-7 mb-auto">
-            <h3 class="text-anchor font-bold mb-6">
+            <h3 class="text-anchor dark:text-accent font-bold mb-6">
                 <a href="<?= base_url($story['full_slug']) ?>"><?= esc($story['name']) ?></a>
             </h3>
             <div class="font-serif"><?= esc($component->summary) ?></div>
-            <div class="text-anchor text-sm mt-6">
+            <div class="text-anchor dark:text-accent text-sm mt-6">
                 <a href="<?= base_url($story['full_slug']) ?>">&gt; checkout</a>
             </div>
         </div>
@@ -31,7 +31,7 @@
      */
     ?>
     <?= $component->_editable ?>
-    <div class="bg-primary dark:bg-neutral-900 h-64 rounded-md flex flex-col" hx-get="<?= base_url('components/featured_post/' . $component->post) ?>" hx-trigger="revealed" hx-swap="outerHTML">
+    <div class="bg-primary dark:bg-neutral-800 h-64 rounded-md flex flex-col" hx-get="<?= base_url('components/featured_post/' . $component->post) ?>" hx-trigger="revealed" hx-swap="outerHTML">
         <div class="pt-5 px-7 mb-auto">
             <h3 class="mb-6 placeholder-loading bg-gray-300 w-32">&nbsp;</h3>
             <div class="mb-1 placeholder-loading bg-gray-300 w-48">&nbsp;</div>
