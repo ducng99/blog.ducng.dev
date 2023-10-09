a3d44a6a358998f819f522381897e8092fd2b530
ducng99 <tom@ducng.dev>
2023-10-09 20:29:58 +1300

    Added missing active state for nav_dropdown

--- app/Views/components/navbar/nav_dropdown.php
+++ app/Views/components/navbar/nav_dropdown.php
@@ -7,12 +7,13 @@ use App\Libraries\Storyblok;
  */
 
 $url = Storyblok::getURLFromLink($component->link);
+$active = base_url(uri_string()) == $url ? 'active' : '';
 ?>
 
 <?= $component->_editable ?>
 <div class="nav_dropdown">
     <a href="<?= esc($url, 'attr') ?>">
-        <div class="nav_item dropdown_btn">
+        <div class="nav_item <?= $active ?> dropdown_btn">
             <?= esc($component->name) ?>
         </div>
     </a>
