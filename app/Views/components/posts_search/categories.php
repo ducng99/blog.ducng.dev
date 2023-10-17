c6da59fee6607d23c03f0cca0bebac26f645d67d
ducng99 <tom@ducng.dev>
2023-10-18 00:19:40 +1300

    Checks child categories on parent clicked

--- app/Views/components/posts_search/categories.php
+++ app/Views/components/posts_search/categories.php
@@ -5,21 +5,21 @@
  * @param int $level
  * @return void
  */
-function generateOptions(array $categories, array $searchParams, int $level = 0): void
+function generateCategorySelections(array $categories, array $searchParams, int $level = 0, string $parent_id = ""): void
 {
     foreach ($categories as $category) :
 ?>
         <div class="px-4 hover:bg-neutral-300 hover:dark:bg-secondary">
             <div class="flex gap-2" style="margin-left: calc(1em * <?= esc($level, 'attr') ?>)">
-                <input id="checkbox_<?= esc($category['item']['uuid'], 'attr') ?>" name="categories[]" type="checkbox" value="<?= esc($category['item']['uuid'], 'attr') ?>" <?= in_array($category['item']['uuid'], $searchParams['categories']) ? 'checked' : '' ?> />
-                <label for="checkbox_<?= esc($category['item']['uuid'], 'attr') ?>" class="grow cursor-pointer">
+                <input id="checkbox_<?= esc($category['item']['id'], 'attr') ?>" name="categories[]" type="checkbox" data-parent="<?= esc($parent_id, 'attr') ?>" hx-on:click="checkChildCategory(this)" value="<?= esc($category['item']['uuid'], 'attr') ?>" <?= in_array($category['item']['uuid'], $searchParams['categories']) ? 'checked' : '' ?> />
+                <label for="checkbox_<?= esc($category['item']['id'], 'attr') ?>" class="grow cursor-pointer">
                     <?= esc($category['item']['name']) ?>
                 </label>
             </div>
         </div>
 <?
         if (!empty($category['children'])) :
-            generateOptions($category['children'], $searchParams, $level + 1);
+            generateCategorySelections($category['children'], $searchParams, $level + 1, $category['item']['uuid']);
         endif;
     endforeach;
 }
@@ -37,7 +37,7 @@ function generateOptions(array $categories, array $searchParams, int $level = 0)
     </label>
     <div class="themable rounded-md overflow-hidden transition-all max-h-0" id="categories-select">
         <div class="max-h-64 py-2 overflow-y-auto">
-            <? generateOptions($categories, $searchParams) ?>
+            <? generateCategorySelections($categories, $searchParams) ?>
         </div>
     </div>
 
@@ -51,4 +51,13 @@ function generateOptions(array $categories, array $searchParams, int $level = 0)
             transform: rotate(90deg);
         }
     </style>
+    <script>
+        function checkChildCategory(parent_element) {
+            let children = document.querySelectorAll(`#categories-select input[data-parent="${parent_element.value}"]`);
+            children.forEach(child => {
+                child.checked = parent_element.checked;
+                checkChildCategory(child);
+            });
+        }
+    </script>
 </div>
